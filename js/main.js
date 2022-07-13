bdd7e76e6f54b04d93b51d94c5fc76e42470c85e
Ezequiel Sanz Zima <ezequielsanzzima@hotmail.com>
2022-07-13 14:21:29 -0300

    v1

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ function redondear(valor){
     return Math.round(valor)
 }
 
+
 for (i=0; i>=0; i--){
     let usuarioPass = prompt('Ingresa la contrasena del instituto para acceder, te quedan '+ (i+1) + ' intentos.')
     if(contrasenaG === usuarioPass){
@@ -21,47 +22,37 @@ if(ingresar){
     let nombreAlumno = prompt('Ingrese nombre y apellido del Alumno');
     
     const notasTrabajos = function (a,b,c) { return a+b+c};
+    const notaFinal = function (a,b) { return a/b}
+    const totalNotas = 3
 
+
+    alert('A continuacion introdusca las 3 notas del alumno: '+ nombreAlumno)
     let notasTrabajosP1 = parseInt(prompt('Ingrese la nota del primer trabajo'));
     let notasTrabajosP2 = parseInt(prompt('Ingrese la nota del segundo trabajo'));
     let notaTrabajoFinal = parseInt(prompt('Ingrese la nota del trabajo final'));
 
-    let notasSumadas = notasTrabajos(notasTrabajosP1,notasTrabajosP2,notaTrabajoFinal)
+    alert('La nota del alumno: '+ nombreAlumno + '\nNota Trabajo 1: '+notasTrabajosP1+ '\nNota trabajo 2: '+notasTrabajosP2+'\nNota trabajo Final: '+ notaTrabajoFinal)
 
-    let resultado = notasSumadas/3
-    alert(notasSumadas);
-    alert(resultado);
+    let notasSumadas = notasTrabajos(notasTrabajosP1,notasTrabajosP2,notaTrabajoFinal)
+    let resultado = notaFinal(notasSumadas,totalNotas)
+    
+    
+    alert(notasSumadas + 'Notas sumadas');
+    alert(resultado + 'resultado');
 
 
     if (resultado>=1 && resultado<=6){
-        alert('Desaprobo :(')
+        alert('Desaprobo :(\nEl alumno:'+nombreAlumno+ 'desaprobo con trimestre con: '+resultado)
     }else if(resultado>=7 && resultado<=9){
-        alert('Esta aprobado :)')
+        alert('Esta aprobado :)\nEl alumno: '+ nombreAlumno + ' Esta aprobado con: '+resultado)
     }else if (resultado===10){
-        alert('Esta aprobadisimo!!!! :D')
+        alert('Esta aprobadisimo!!!! :D\nNo hace falta decirlo el alumno: '+nombreAlumno+'Se saco un 10!!!!')
     }else{
         alert('ERROR!!!')
     }
 
+}else{
+    alert('La cuenta se bloqueo, por muchos intentos.')
 }
-
-//Arreglar texto
-
-
-
-
-
-/* let menu = parseInt(prompt('A continuacion ingrese: \n-1 para vender.\n-2 para comprar.'));
-while(menu != 'ESC'){
-    switch(menu){
-        case "1":
-            alert('Vender')
-            break;
-        case "2":
-            alert('comprar')
-            break;
-    }
-    menu = parseInt(prompt('A continuacion ingrese: \n-1 para vender.\n-2 para comprar.'));
-} */
 
 
